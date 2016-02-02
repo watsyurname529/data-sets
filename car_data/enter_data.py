1fde303d76e13b0dba4043fc6c81027827176a06
Andrew <watsyurname529@yahoo.com>
2016-02-01 22:49:37 -0500

    Added new data set
    Added a new data set of performace data of the CRV. A very simple python script was made to speed up data entry. Data is saved in a CSV file, intended to be usd with R.

---
+++ car_data/enter_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import csv
+
+file_name = input('Enter name of save file: ')
+date = ''
+
+with open(file_name, 'a') as csvfile:
+    car_data = csv.writer(csvfile)
+    while(True):
+        date = input('Date: ')
+        miles = input('Miles: ')
+        gal = input('Gallons: ')
+        money = input('Money: ')
+        if(date == 'quit'):
+            break
+
+        car_data.writerow([date, miles, gal, money])
